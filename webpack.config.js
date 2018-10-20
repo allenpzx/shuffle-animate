e779cf717494933899f441a84c05fd421dc4774d
彭子修 <pengzixiu@pengzixiudeAir.lan>
2018-10-20 10:54:26 +0800

    save

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
-        library: 'shuffleAnimate',
+        library: 'ShuffleAnimate',
         libraryTarget: 'umd'
     },
     module: {
